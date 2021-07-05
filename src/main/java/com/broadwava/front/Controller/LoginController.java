0be3b01f1ebd03dc37469333f347aa52692a30fd
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-07-05 13:41:26 +0900

    로그인 url 변경

--- src/main/java/com/broadwava/front/Controller/LoginController.java
+++ src/main/java/com/broadwava/front/Controller/LoginController.java
@@ -3,14 +3,20 @@ package com.broadwava.front.Controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class LoginController {
 
+    // 호스트링크주입
+    @Value("${newdeal.api.security_url}")
+    private String security_url;
+
     @RequestMapping("login")
-    public String loginForm(){
+    public String loginForm(Model model){
+        model.addAttribute("security_url", security_url);
         return "loginForm";
     }
 
