858fa5bf8598f6215bb596cf89a99fc40123403c
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-07-07 09:46:29 +0900

    커밋

--- src/main/java/com/broadwava/front/Controller/MainController.java
+++ src/main/java/com/broadwava/front/Controller/MainController.java
@@ -1,12 +1,18 @@
 package com.broadwava.front.Controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class MainController {
+
+    // 호스트링크주입
+    @Value("${newdeal.api.backend_url}")
+    private String backend_url;
 
     @RequestMapping("index")
     public String index(){
@@ -17,6 +23,15 @@ public class MainController {
     @RequestMapping("newpage")
     public String newpage1(){
         return "page/newpage";
+    }
+
+    @RequestMapping("/mypage")
+    public String mypage(Model model){
+        model.addAttribute("backend_url", backend_url);
+//        String userid = CommonUtils.getCurrentuser(request);
+//        Account account = accountService.findByUserid(userid).get();
+//        model.addAttribute("account",account);
+        return "mypage";
     }
 
     @RequestMapping("performance1")
